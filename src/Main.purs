6e5cf065b6e74b37d7b534dc71b892918440c9e2
George Varghese M <georgevarghese185@yahoo.com>
2017-06-10 16:02:32 +0530

    Cube elements generation
    Added PureScript code that generates cube elements. No styling done yet

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,52 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.JQuery (addClass, append, body, create, setAttr)
+import DOM (DOM)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+generateCube :: forall e. Eff (dom :: DOM |e) Unit
+generateCube = do
+  frontFace <- create "<div>"
+  setAttr "id" "front_face" frontFace
+  addClass "face" frontFace
+
+  backFace <- create "<div>"
+  setAttr "id" "back_face" backFace
+  addClass "face" backFace
+
+  rightFace <- create "<div>"
+  setAttr "id" "right_face" rightFace
+  addClass "face" rightFace
+
+  leftFace <- create "<div>"
+  setAttr "id" "left_face" leftFace
+  addClass "face" leftFace
+
+  topFace <- create "<div>"
+  setAttr "id" "top_face" topFace
+  addClass "face" topFace
+
+  bottomFace <- create "<div>"
+  setAttr "id" "bottom_face" bottomFace
+  addClass "face" bottomFace
+
+  cube <- create "<div>"
+  addClass "cube" cube
+
+  append frontFace cube
+  append backFace cube
+  append rightFace cube
+  append leftFace cube
+  append topFace cube
+  append bottomFace cube
+
+  cubeWrapper <- create "<div>"
+  setAttr "id" "cube-wrapper" cubeWrapper
+  append cube cubeWrapper
+
+  body <- body
+  append cubeWrapper body
+
+
+main :: forall e. Eff (dom :: DOM | e) Unit
+main = generateCube
