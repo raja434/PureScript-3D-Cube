2a9fa14a26a9875cd94945f57fc50224b6db27b6
George Varghese M <georgevarghese185@yahoo.com>
2017-06-25 19:43:44 +0530

    Added style for cube faces
    Added PureScript code for applying styles to the cube faces.
    Gave cube faces a colour to prevent depth ambiguity.
    Imported append function of JQuery with JQ prefix to avoid shadowed 
    defitions warning.

--- src/Main.purs
+++ src/Main.purs
@@ -2,7 +2,8 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.JQuery (addClass, append, body, create, setAttr)
+import Control.Monad.Eff.JQuery (addClass, body, create, css, select, setAttr)
+import Control.Monad.Eff.JQuery (append) as JQ
 import DOM (DOM)
 
 generateCube :: forall e. Eff (dom :: DOM |e) Unit
@@ -34,19 +35,59 @@ generateCube = do
   cube <- create "<div>"
   addClass "cube" cube
 
-  append frontFace cube
-  append backFace cube
-  append rightFace cube
-  append leftFace cube
-  append topFace cube
-  append bottomFace cube
+  css {
+  	transform : "translateX(-100px) translateY(-100px) translateZ(100px)"
+  } frontFace
+
+  css {
+  	transform : "translateX(-100px) translateY(-100px) translateZ(-100px)"
+  } backFace
+  css {
+  	transform : "translateY(-100px) rotateY(90deg)"
+  } rightFace
+  css {
+    transform : "translateY(-100px) translateX(-200px) rotateY(90deg)"
+  } leftFace
+  css {
+    transform : "translateX(-100px) translateY(-200px) rotateX(90deg)"
+  } topFace
+  css {
+    transform : "translateX(-100px) rotateX(90deg)"
+  } bottomFace
+
+  css {
+    position: "relative",
+  	transformStyle: "preserve-3d"
+  } cube
+
+  JQ.append frontFace cube
+  JQ.append backFace cube
+  JQ.append rightFace cube
+  JQ.append leftFace cube
+  JQ.append topFace cube
+  JQ.append bottomFace cube
 
   cubeWrapper <- create "<div>"
   setAttr "id" "cube-wrapper" cubeWrapper
-  append cube cubeWrapper
+  css {
+    position : "absolute",
+    left : "50%",
+    top : "50%",
+    perspective: "1500px"
+  } cubeWrapper
+  JQ.append cube cubeWrapper
 
   body <- body
-  append cubeWrapper body
+  JQ.append cubeWrapper body
+
+  face <- select ".face"
+  css {
+  	position : "absolute",
+  	width : "200px",
+  	height : "200px",
+    border : "solid green 3px",
+    backgroundColor : "rgba(0, 192, 255, 0.17)"
+  } face
 
 
 main :: forall e. Eff (dom :: DOM | e) Unit
