d42967b508f51aeac9bd0bdf6c21ab5e637e95c4
George Varghese M <georgevarghese185@yahoo.com>
2017-06-26 15:33:13 +0530

    Spinning function added
    -Created a function 'run' that will set up all handlers and timed loops
    for the interactive functions of the cube.
    
    -'run' currently creates local mutable references to variables holding
    the cube's current rotation angles and velocities at which it should
    rotate and it calls 'startSpinner' function
    
    -'startSpinner' uses requestAnimationFrame to constantly rotate the cube
    at the given velocity and updates the cubes angles accordingly.
    
    -Renamed 'generateCube' to 'drawCube'

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,27 @@
 module Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.JQuery (addClass, body, create, css, select, setAttr)
 import Control.Monad.Eff.JQuery (append) as JQ
+import Control.Monad.ST (ST, STRef, newSTRef, readSTRef, writeSTRef)
 import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Window (requestAnimationFrame)
 
-generateCube :: forall e. Eff (dom :: DOM |e) Unit
-generateCube = do
+type Velocities = {
+  x :: Number,
+  y :: Number
+}
+
+type Angles = {
+  x :: Number,
+  y :: Number
+}
+
+drawCube :: forall e. Eff (dom :: DOM |e) Unit
+drawCube = do
   frontFace <- create "<div>"
   setAttr "id" "front_face" frontFace
   addClass "face" frontFace
@@ -90,5 +104,39 @@ generateCube = do
   } face
 
 
-main :: forall e. Eff (dom :: DOM | e) Unit
-main = generateCube
+startSpinner :: forall h e. STRef h {x::Number, y::Number}
+  -> STRef h {x::Number, y::Number}
+  -> Eff (dom :: DOM, st :: ST h | e) Unit
+startSpinner angleRef velocityRef = do
+  let
+    spinner = do
+      cube <- select ".cube"
+      vel <- readSTRef velocityRef
+      angle <- readSTRef angleRef
+      let
+        newAngle = {
+          x: angle.x + (vel.x / 60.0),
+          y: angle.y + (vel.y / 60.0)
+        }
+      css {
+        transform: "rotateX(" <> show(newAngle.x) <> "deg)"
+                      <> " rotateY(" <> show(newAngle.y) <> "deg)"
+      } cube
+      void $ writeSTRef angleRef newAngle
+      w <- window
+      void $ requestAnimationFrame spinner w
+  spinner
+  pure unit
+
+
+run :: forall e h. Eff (dom :: DOM, st :: ST h | e) Unit
+run = do
+  angleRef <- newSTRef { x: 0.0, y: 0.0 }
+  velocityRef <- newSTRef { x: 60.0, y: 60.0 }
+  startSpinner angleRef velocityRef
+
+
+main :: forall h e. Eff (dom :: DOM, st :: ST h | e) Unit
+main = do
+  drawCube
+  run
