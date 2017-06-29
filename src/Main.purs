6022c206f27aa982b5d905a23ca51a823180d30e
George Varghese M <georgevarghese185@yahoo.com>
2017-06-29 22:38:58 +0530

    Mouse rotation added
    Added functions to allow cube to be rotated by user interaction
    
    -startMouseHandlers function registers a handler for mousedown.
    
    -downHandler handles when the moue is clicked. It registers a move 
    handler to rotate the cube as the mouse is moved and registers an 
    upHandler to cancel the move handler when mouseup occurs
    
    -rotateCube takes a new angle and reference to the mutable angle and 
    rotates the cube followed by saving the new angle.

--- src/Main.purs
+++ src/Main.purs
@@ -3,22 +3,13 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.JQuery (addClass, body, create, css, select, setAttr)
+import Control.Monad.Eff.JQuery (addClass, body, create, css, getPageX, getPageY, on, off, select, setAttr)
 import Control.Monad.Eff.JQuery (append) as JQ
 import Control.Monad.ST (ST, STRef, newSTRef, readSTRef, writeSTRef)
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Window (requestAnimationFrame)
 
-type Velocities = {
-  x :: Number,
-  y :: Number
-}
-
-type Angles = {
-  x :: Number,
-  y :: Number
-}
 
 drawCube :: forall e. Eff (dom :: DOM |e) Unit
 drawCube = do
@@ -104,36 +95,75 @@ drawCube = do
   } face
 
 
+rotateCube :: forall h e. STRef h {x::Number, y::Number}
+  -> {x::Number, y::Number}
+  -> Eff (dom :: DOM, st :: ST h |e) Unit
+rotateCube angleRef newAngle = do
+  cube <- select ".cube"
+  angle <- readSTRef angleRef
+  css {
+    transform: "rotateX(" <> show(newAngle.x) <> "deg)"
+                  <> " rotateY(" <> show(newAngle.y) <> "deg)"
+  } cube
+  void $ writeSTRef angleRef newAngle
+
+
+startMouseHandlers :: forall h e. STRef h {x::Number, y::Number}
+  -> STRef h {x::Number, y::Number}
+  -> Eff (dom :: DOM, st :: ST h | e) Unit
+startMouseHandlers angleRef velocityRef = do
+  lastMousePos <- newSTRef {x: 0.0, y: 0.0}
+  body <- body
+  let moveHandler event jq = do
+        x <- getPageX event
+        y <- getPageY event
+        lastPos <- readSTRef lastMousePos
+        angle <- readSTRef angleRef
+        let newAngle = {
+              x: angle.x + negate (y - lastPos.y),
+              y: angle.y + x - lastPos.x
+            }
+        rotateCube angleRef newAngle
+        void $ writeSTRef lastMousePos {x: x, y: y}
+  let upHandler event jq = do
+        off "mousemove" body
+  let downHandler event jq = do
+        lastX <- getPageX event
+        lastY <- getPageY event
+        void $ writeSTRef lastMousePos {x: lastX, y: lastY}
+        on "mousemove" moveHandler body
+        on "mouseup" upHandler body
+  css {
+    width: "100%",
+    height: "100%"
+  } body
+  on "mousedown" downHandler body
+
+
 startSpinner :: forall h e. STRef h {x::Number, y::Number}
   -> STRef h {x::Number, y::Number}
   -> Eff (dom :: DOM, st :: ST h | e) Unit
 startSpinner angleRef velocityRef = do
-  let
-    spinner = do
-      cube <- select ".cube"
-      vel <- readSTRef velocityRef
-      angle <- readSTRef angleRef
-      let
-        newAngle = {
-          x: angle.x + (vel.x / 60.0),
-          y: angle.y + (vel.y / 60.0)
-        }
-      css {
-        transform: "rotateX(" <> show(newAngle.x) <> "deg)"
-                      <> " rotateY(" <> show(newAngle.y) <> "deg)"
-      } cube
-      void $ writeSTRef angleRef newAngle
-      w <- window
-      void $ requestAnimationFrame spinner w
+  let spinner = do
+        vel <- readSTRef velocityRef
+        angle <- readSTRef angleRef
+        let
+          newAngle = {
+            x: angle.x + (vel.x / 60.0),
+            y: angle.y + (vel.y / 60.0)
+          }
+        rotateCube angleRef newAngle
+        w <- window
+        void $ requestAnimationFrame spinner w
   spinner
-  pure unit
 
 
 run :: forall e h. Eff (dom :: DOM, st :: ST h | e) Unit
 run = do
   angleRef <- newSTRef { x: 0.0, y: 0.0 }
-  velocityRef <- newSTRef { x: 60.0, y: 60.0 }
+  velocityRef <- newSTRef { x: 0.0, y: 0.0 }
   startSpinner angleRef velocityRef
+  startMouseHandlers angleRef velocityRef
 
 
 main :: forall h e. Eff (dom :: DOM, st :: ST h | e) Unit
